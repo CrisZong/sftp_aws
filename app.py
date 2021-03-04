cc260ed55aca6c5023f77bdae2d0656d34035d6b
yizong98 <yizong@ucsd.edu>
2021-03-04 14:06:26 -0800

    add flask endpoint

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify, make_response
 import requests
 import json
-from dateutil.parser import parse
 import datetime
 import subprocess
 import sys
 import os
 app = Flask(__name__)
+
+def generateResponse(elem={}, status=200):
+    response = make_response(jsonify(elem), status)
+    return response
 
 def sftp_bash():
     print(os.geteuid(),"current user id in drop in")
@@ -16,37 +19,28 @@ def sftp_bash():
     message = "All most recent files are dropped" if exit_code else "Successful drop in to the HS_COVID_WW"
     return message
 
-def is_date(string, fuzzy=False):
-    """
-    Return whether the string can be interpreted as a date.
-
-    :param string: str, string to check for date
-    :param fuzzy: bool, ignore unknown tokens in string if True
-    """
-    try: 
-        parse(string, fuzzy=fuzzy)
-        return True
-
-    except ValueError:
-        return False
 
 @app.route('/',methods = ['GET'])
 def drop_in_file():
     today = datetime.date.today()
     prevday = today - datetime.timedelta(days=1)
     date_val = request.args.get('date') or prevday.strftime('%-m/%-d')
-    if is_date(date_val): # if the date value inputted is valid
-        year = datetime.date.today().year
-        name_pattern = datetime.datetime.strptime(date_val,"%m/%d").strftime('%b%-d')
+    try:
+        date_val = datetime.datetime.strptime(date_val,"%m/%d").strftime("%-m/%-d")
+        name_pattern = datetime.datetime.strptime(date_val,"%m/%d").strftime("%b%-d")
         saved_path = 'data/WW_%s.csv'%(name_pattern)
         r = requests.post('https://4jzevgh86d.execute-api.us-east-1.amazonaws.com/default/traceAPI',
                         data='{"password": "Open, sesame","date":"%s","mode":"drop"}'%date_val,
                         headers={"Content-Type":"application/json"})
+        if  r.status_code != requests.codes.ok:
+            return generateResponse({"message":"fetching drop in files fail, please try again later"},406)
         with open(saved_path,'w') as f:
             f.write(json.loads(r.text))
-        message = sftp_bash()
-        return "Finished File upload for date %s " % date_val + "sftp status: %s" % message
-    return "the date entered isn't valid"
+        sftp_message = sftp_bash()
+        return_message = "Finished File upload for date %s " % date_val + "sftp status: %s" % sftp_message
+        return generateResponse({"message":return_message})
+    except:
+        return generateResponse({"message":"the date entered isn't valid"},400)
 if __name__ == '__main__':
     app.secret_key = b'\xb7\xe2\xd6\xa3\xe2\xe0\x11\xd1\x92\xf1\x92G&>\xa2:'
     app.debug = True
